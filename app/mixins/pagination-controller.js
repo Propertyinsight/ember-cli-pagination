05971b5376a4f43e662315df24b7c60696ae7227
Johnny Oshika <johnnyoshika@gmail.com>
2014-11-04 23:54:32 -0800

    Forcing fetch of list on first route load.  The last commit didn't work.

--- app/mixins/pagination-controller.js
+++ app/mixins/pagination-controller.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
 
     queryParams: ['q', 'page', 'pageSize'],
-    q: '',
+    q: null,
     page: 1,
     pageSize: 10,
 
--- app/mixins/pagination-route.js
+++ app/mixins/pagination-route.js
@@ -2,10 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 
+    fetched: false,
+
     queryParams: {
         q: {
             replace: true
         }
+    },
+
+    setupController: function(controller, model) {
+        this._super(controller, model);
+
+        if (!this.get('fetched'))
+            controller.filter();
+
+        this.set('fetched', true);
     }
 
 });
