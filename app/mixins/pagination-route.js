89d842bff324429b5ca9cb436385c9ff0994e1b6
Johnny Oshika <johnnyoshika@gmail.com>
2014-11-04 23:49:54 -0800

    Not longer forcing 'refreshModel' on query params because they don't do anything in our situation.

--- app/mixins/pagination-route.js
+++ app/mixins/pagination-route.js
@@ -4,11 +4,7 @@ export default Ember.Mixin.create({
 
     queryParams: {
         q: {
-            refreshModel: true,
             replace: true
-        },
-        page: {
-            refreshModel: true
         }
     }
 
