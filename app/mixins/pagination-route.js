2f98478dc8a3271f919959b563902c67d342452e
Johnny Oshika <johnnyoshika@gmail.com>
2014-11-05 18:15:45 -0800

    No longer eagerly fetching paginated data.

--- app/mixins/pagination-route.js
+++ app/mixins/pagination-route.js
@@ -2,21 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 
-    fetched: false,
-
     queryParams: {
         q: {
             replace: true
         }
-    },
-
-    setupController: function(controller, model) {
-        this._super(controller, model);
-
-        if (!this.get('fetched'))
-            controller.filter();
-
-        this.set('fetched', true);
     }
 
 });
